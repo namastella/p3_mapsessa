949424fedde1da97f63792b305cc3bd9052318d1
namastella <stella.umlauf@stud.h-da.de>
2026-01-21 12:08:59 +0100

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import { Map } from './components/Map/Map.jsx'
+import './App.css'
+
+function App() {
+//  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hallo mapsessa!</h1>
+
+      <Map />
+    </>
+
+    
+  )
+}
+
+export default App
---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,16 @@
+import "./Map.css"
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+
+
+export function Map () {
+    return (
+        <MapContainer center={[49.8728, 8.6512]} zoom={15}> {/* center=  per default Darmstadt (in Klammern Koordinaten) zoom ist wie weit reingezoomt ist*/}
+            <TileLayer 
+                attribution= '&copy; <a href="https://www.openstreetmap.org/copyright" >OpenStreetMap</a>'
+                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        </MapContainer>
+    )
+}
