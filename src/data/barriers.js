50030ee04c463467c1b7701d932230d463ea2907
namastella <stella.umlauf@stud.h-da.de>
2026-02-05 18:04:17 +0100

    dark mode changes

---
+++ src/data/barriers.js
@@ -0,0 +1,23 @@
+export const BARRIERS = [
+  {
+    id: "b1",
+    title: "Kaputter Bordstein",
+    type: "Bordstein",
+    position: [49.87295, 8.6519],
+    description: "Uneben, schwer passierbar (Rollstuhl/Kinderwagen).",
+  },
+  {
+    id: "b2",
+    title: "Starkes Gefälle",
+    type: "Gefälle",
+    position: [49.8734, 8.6509],
+    description: "Sehr steil – allein anstrengend.",
+  },
+  {
+    id: "b3",
+    title: "Baustelle / Engstelle",
+    type: "Baustelle",
+    position: [49.8723, 8.6526],
+    description: "Schmaler Durchgang, zeitweise blockiert.",
+  },
+];
